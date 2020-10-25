19cd9934c96067fcce7e3493a9e3bd38ffaa14e4
vrshchk <maryvaresh12@gmail.com>
2020-10-25 19:32:19 +0200

    Updates

---
+++ precache-manifest.d5a07fd11fb7a947cfcd20c982a7c4de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8c9277af029efc76e2f4c856dd6d2c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1d0771ba567b848eaabe",
+    "url": "/monsters-rolodex/static/css/main.ba49b822.chunk.css"
+  },
+  {
+    "revision": "0ebb722af33ec599140e",
+    "url": "/monsters-rolodex/static/js/2.c27b6d93.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/monsters-rolodex/static/js/2.c27b6d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0771ba567b848eaabe",
+    "url": "/monsters-rolodex/static/js/main.22802013.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
